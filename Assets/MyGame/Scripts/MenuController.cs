8105b7f298456955806065cfdb6208971ed3a9c5
Jonas Riepler <jonas.riepler@htl-salzburg.ac.at>
2019-12-16 21:00:37 +0100

    Finished Refactoring MenuController.cs by putting in useful Comments to understand the code

--- Assets/MyGame/Scripts/MenuController.cs
+++ Assets/MyGame/Scripts/MenuController.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour{
+    //If Play is called, it loads the MainScene via the SceneManager
     public void Play(){
         SceneManager.LoadScene("MainScene");
     }
-
+    //If Exit is called, it quits the Application if Built
     public void Exit()
     {
         Application.Quit();
